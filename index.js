cb9fcd123b58eb2685ca43107bf250410f1c639f
Ryan <Ryan@localhost.com>
2024-06-17 11:09:25 +0300

    Add the GET and POST rest api methods

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+app.use(express.json());
+const port = 8000;
 const sqlite3 = require('sqlite3').verbose();
 const dotenv = require('dotenv').config();
 const db = new sqlite3.Database(process.env.DB_PATH);
@@ -9,6 +10,77 @@ const db = new sqlite3.Database(process.env.DB_PATH);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.get('/api/cars', (req, res) => {
+    let offset = req.query.offset || 0;
+    let limit = req.query.limit || 10;
+    let data = [];
+    db.serialize(() => {
+        db.each(`
+            SELECT * FROM cars
+            LIMIT ${limit} OFFSET ${offset}
+        `, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            data.push(row);
+        }, () => {
+            res.send(data);
+        })
+    })
+    let count = 0;
+    db.serialize(() => {
+        db.each(`
+            SELECT COUNT(*) FROM cars
+        `, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            count = row['COUNT(*)'];
+        }, () => {
+            console.log(`Total number of cars: ${count}`);
+        })
+    })
+    return {
+        "data": data,
+        "count": count
+    }
+
+})
+
+app.get('/api/cars/:id', (req, res) => {
+    let id = req.params.id;
+    let data = {};
+    db.serialize(() => {
+        db.each(`
+            SELECT * FROM cars
+            WHERE id = ${id}
+        `, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            data = row;
+        }, () => {
+            res.send(data);
+        })
+    })
+    return data;
+})
+
+app.post('/api/cars', (req, res) => {
+    // The data is JSON encoded
+    let data = req.body;
+    db.serialize(() => {
+        db.run(`
+            INSERT INTO cars (brand, model, registrationNumber, notes, document, image)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `, [data.brand, data.model, data.registrationNumber, data.notes, data.document, data.image])
+    })
+    res.send('Car added successfully!');
+    /** Example data =
+     *
+     */
 })
 
 app.listen(port, () => {
